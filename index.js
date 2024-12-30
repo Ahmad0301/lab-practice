9b463b5c4d3c50cfed5ec40258feda17bd567d6e
Ahmad0301 <sehroz3040@gmail.com>
2024-12-31 02:41:48 +0500

    require book model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ('express')
 const mongoose = require('mongoose')
 const app = express()
 const db = require('./connection')
+const book = require('./models/bookmodel')
 app.use(express.json())
 app.listen(3000,()=>{
     console.log('server is connected')
