da7ac07232b3eaab5b5e9a07e400607cf086343a
Ahmad0301 <sehroz3040@gmail.com>
2025-01-05 20:05:18 +0500

    passport library

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const db = require('./connection')
 const book = require('./models/bookmodel')
 const user  =require('./models/usermodel')
 app.use(express.json())
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to ${req.originalUrl}`) 
+    next();
+    }
+    app.use(logRequest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000,()=>{
     console.log('server is connected')
 })
